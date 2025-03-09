7e000d88befd86427c571c7bbdad7b1626d5d684
xSmilO <mateosmigiel2005@gmail.com>
2025-03-09 22:28:09 +0100

    small rate chart changes

--- lib/UI/Home/viewmodels/rate_slider_viewmodel.dart
+++ lib/UI/Home/viewmodels/rate_slider_viewmodel.dart
@@ -4,18 +4,13 @@ import 'package:jak_sie_masz/Data/rate_slider_repository.dart';
 class RateSliderViewmodel extends ChangeNotifier {
   final RateSliderRepository rateSliderRepository;
   int currentIndex = 0;
-  double pointerPosition = 0;
   RateSliderViewmodel({required this.rateSliderRepository});
 
   void setCurrentIndex(int index) {
     if (index != rateSliderRepository.index) {
-      print("new index $index");
       rateSliderRepository.setIndex(index);
       currentIndex = index;
-      calculatePointerPosition();
       notifyListeners();
     }
   }
-
-  void calculatePointerPosition() {}
 }
